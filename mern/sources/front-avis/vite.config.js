66bca59f01563d945eaa5bdb221d0e80a359c933
ZauggL <zaugg.loic@gmail.com>
2024-01-09 16:22:50 +0100

    Ajout de variables d'environnement pour la partie avant.

--- mern/sources/front-avis/vite.config.js
+++ mern/sources/front-avis/vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const service = process.env.NODE_SERVICE ;
+const port = process.env.NODE_PORT ;
+const nodeURL = `http://${service}:${port}`;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://node-back-service:8000',
+        target: nodeURL,
         changeOrigin: true
       },
     }
